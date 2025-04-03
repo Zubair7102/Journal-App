311bab71ea4a64f7ba9f31759839bc649dcad37a
Zubair7102 <jubaira646@gmail.com>
2025-04-03 16:07:46 +0530

    Rest Methods

--- src/main/java/com/tothenew/journalApp/controller/HealthCheck.java
+++ src/main/java/com/tothenew/journalApp/controller/HealthCheck.java
@@ -1,4 +1,15 @@
 package com.tothenew.journalApp.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HealthCheck {
+
+    @GetMapping("/health-check")
+    public String healthCheck()
+    {
+        return "OK";
+    }
+
 }
--- src/main/java/com/tothenew/journalApp/controller/JournalEntryController.java
+++ src/main/java/com/tothenew/journalApp/controller/JournalEntryController.java
@@ -1,4 +1,32 @@
 package com.tothenew.journalApp.controller;
+import com.tothenew.journalApp.entity.JournalEntry;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/* Controllers are special type of classes or components that handle http requests */
+@RestController /* @RestController: This tells Spring Boot that this class will handle HTTP requests and return data in JSON format. */
+@RequestMapping("/journal")
 public class JournalEntryController {
+
+    private Map<Long, JournalEntry> journalEntries = new HashMap<>();
+    /* The key is Long (entry ID), and the value is a JournalEntry object. */
+
+    /* Methods inside a controller class should be public so that they can be accessed and invoked by the Spring FrameWork or External HTTP requests */
+
+    @GetMapping
+    public List<JournalEntry> getAll(){
+        return new ArrayList<>(journalEntries.values()); /* this Converts the values from the HashMap into a list and returns them.*/ 
+    }
+
+    @PostMapping
+    public boolean createEntry(@RequestBody JournalEntry myEntry)
+            /* By writing @RequestBody we are saying "Hey Spring" please take the data from the request and turn it into a java object (here JournalEntry) that we can use in code  */
+    {
+        journalEntries.put(myEntry.getId(), myEntry);
+        return true;
+    }
 }
