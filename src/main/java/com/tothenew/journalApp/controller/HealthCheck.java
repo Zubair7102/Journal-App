3339735c843527fe02ed685f1a30d1c68792b698
Zubair7102 <jubaira646@gmail.com>
2025-04-04 00:54:25 +0530

    Code Explain Comments

--- src/main/java/com/tothenew/journalApp/controller/HealthCheck.java
+++ src/main/java/com/tothenew/journalApp/controller/HealthCheck.java
@@ -6,10 +6,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HealthCheck {
 
-    @GetMapping("/health-check")
-    public String healthCheck()
+    @GetMapping("/health-check") // This method will be called when a GET request is made to the "/health-check" endpoint.
+    /* @GetMapping: This annotation is used to map HTTP GET requests to a specific method in the controller. */
+    public String healthCheck() 
     {
-        return "OK";
+        return "OK"; 
     }
 
 }
--- src/main/java/com/tothenew/journalApp/controller/JournalEntryController.java
+++ src/main/java/com/tothenew/journalApp/controller/JournalEntryController.java
@@ -12,21 +12,26 @@ import java.util.Map;
 @RequestMapping("/journal")
 public class JournalEntryController {
 
-    private Map<Long, JournalEntry> journalEntries = new HashMap<>();
+    private Map<Long, JournalEntry> journalEntries = new HashMap<>(); 
     /* The key is Long (entry ID), and the value is a JournalEntry object. */
 
     /* Methods inside a controller class should be public so that they can be accessed and invoked by the Spring FrameWork or External HTTP requests */
 
     @GetMapping
     public List<JournalEntry> getAll(){
-        return new ArrayList<>(journalEntries.values()); /* this Converts the values from the HashMap into a list and returns them.*/ 
+        return new ArrayList<>(journalEntries.values()); 
+        /* This method returns a list of all journal entries. */
+        /* The values() method returns a collection of all the values (JournalEntry objects) in the HashMap. */
+        /* The ArrayList constructor takes a collection as an argument and creates a new ArrayList containing the elements of that collection. */
+        /* this Converts the values from the HashMap into a list and returns them.*/   
     }
 
     @PostMapping
     public boolean createEntry(@RequestBody JournalEntry myEntry)
             /* By writing @RequestBody we are saying "Hey Spring" please take the data from the request and turn it into a java object (here JournalEntry) that we can use in code  */
     {
-        journalEntries.put(myEntry.getId(), myEntry);
-        return true;
+        journalEntries.put(myEntry.getId(), myEntry); /* This line adds the new entry to the journalEntries map using the entry ID as the key. */
+        /* The put method is used to add a new entry to the HashMap. It takes two arguments: the key (entry ID) and the value (JournalEntry object). */ 
+        return true; /* This method returns true if the entry is created successfully. */
     }
 }
