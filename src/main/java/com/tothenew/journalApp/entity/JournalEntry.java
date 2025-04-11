d8b902e67cfece3ad77aa9eddd7d465d6cc87d52
Zubair7102 <jubaira646@gmail.com>
2025-04-11 16:16:28 +0530

    docker & user collection

--- src/main/java/com/tothenew/journalApp/entity/JournalEntry.java
+++ src/main/java/com/tothenew/journalApp/entity/JournalEntry.java
@@ -2,6 +2,7 @@ package com.tothenew.journalApp.entity;
 
 import lombok.Data;
 import lombok.Getter;
+import lombok.NonNull;
 import lombok.Setter;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
@@ -19,6 +20,7 @@ public class JournalEntry {
     @Id
     private ObjectId id;
 
+    @NonNull
     private String title;
 
     private String content;
--- src/main/java/com/tothenew/journalApp/entity/User.java
+++ src/main/java/com/tothenew/journalApp/entity/User.java
@@ -1,15 +1,28 @@
 package com.tothenew.journalApp.entity;
 
+import lombok.NonNull;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Document(collection = "users")
 public class User {
 
     @Id private ObjectId id;
 
-    private String userName;
+    @Indexed(unique = true) //this will ensure to keep unique username
 
+    @NonNull
+    private String userName;
+    @NonNull
     private String password;
+
+    @DBRef /* The @DBRef annotation is used to create a reference (relationship) between two MongoDB documents @DBRef tells MongoDB:
+    "This field links to another collection && this creates a one-to-many relationship: One User → Many Journal Entries*/
+    private List<JournalEntry> journalEntries = new ArrayList<>();
 }
