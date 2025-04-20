2fe195843932cfe5483e4c9999fb9c900c3a8393
Zubair7102 <jubaira646@gmail.com>
2025-04-20 16:49:37 +0530

    logic update

--- src/main/java/com/tothenew/journalApp/controller/JournalEntryControllerV2.java
+++ src/main/java/com/tothenew/journalApp/controller/JournalEntryControllerV2.java
@@ -1,7 +1,9 @@
 package com.tothenew.journalApp.controller;
 
 import com.tothenew.journalApp.entity.JournalEntry;
+import com.tothenew.journalApp.entity.User;
 import com.tothenew.journalApp.service.JournalEntryService;
+import com.tothenew.journalApp.service.UserService;
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -19,10 +21,14 @@ public class JournalEntryControllerV2 {
     @Autowired
     private JournalEntryService journalEntryService;
 
+    @Autowired
+    private UserService userService;
+
 //    method to get all entries
-    @GetMapping
-    public ResponseEntity<List<JournalEntry>> getAll() {
-        List<JournalEntry> allEntry =  journalEntryService.getAll();
+    @GetMapping("{userName}")
+    public ResponseEntity<List<JournalEntry>> getAllJournalEntriesOfUser(@PathVariable String userName) {
+        User user = userService.findByUserName(userName);
+        List<JournalEntry> allEntry =  user.getJournalEntries();
         if(allEntry != null && !allEntry.isEmpty())
         {
             return new ResponseEntity<>(allEntry, HttpStatus.OK);
@@ -33,11 +39,12 @@ public class JournalEntryControllerV2 {
     }
 
 //    method to create a new entry
-    @PostMapping
-    public ResponseEntity<JournalEntry> createEntry(@RequestBody JournalEntry myEntry) {
+    @PostMapping("{userName}")
+    public ResponseEntity<JournalEntry> createEntry(@RequestBody JournalEntry myEntry, @PathVariable String userName) {
         try{
+
             myEntry.setDate(LocalDateTime.now());
-            journalEntryService.saveEntry(myEntry);
+            journalEntryService.saveEntry(myEntry, userName);
             return new ResponseEntity<>(myEntry, HttpStatus.CREATED);
         }
         catch (Exception e)
@@ -71,19 +78,19 @@ public class JournalEntryControllerV2 {
     }
 
 //    method to update an existing entry by its Id
-    @PutMapping("/id/{id}")
-    public ResponseEntity<JournalEntry> updateJournalById(@PathVariable ObjectId id, @RequestBody JournalEntry updatedEntry) {
-        JournalEntry oldEntry = journalEntryService.findById(id).orElse(null);
-        if(oldEntry != null) {
-            oldEntry.setTitle(updatedEntry.getTitle() != null && !updatedEntry.getTitle().equals("") ? updatedEntry.getTitle() : oldEntry.getTitle());
-            oldEntry.setContent(updatedEntry.getContent() != null && !updatedEntry.equals("") ? updatedEntry.getContent() : oldEntry.getContent());
-
-            journalEntryService.saveEntry(oldEntry);
-            return new ResponseEntity<>(oldEntry, HttpStatus.OK);
-        }
-        else{
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
-
-    }
+//    @PutMapping("/id/{id}")
+//    public ResponseEntity<JournalEntry> updateJournalById(@PathVariable ObjectId id, @RequestBody JournalEntry updatedEntry) {
+//        JournalEntry oldEntry = journalEntryService.findById(id).orElse(null);
+//        if(oldEntry != null) {
+//            oldEntry.setTitle(updatedEntry.getTitle() != null && !updatedEntry.getTitle().equals("") ? updatedEntry.getTitle() : oldEntry.getTitle());
+//            oldEntry.setContent(updatedEntry.getContent() != null && !updatedEntry.equals("") ? updatedEntry.getContent() : oldEntry.getContent());
+//
+//            journalEntryService.saveEntry(oldEntry, user);
+//            return new ResponseEntity<>(oldEntry, HttpStatus.OK);
+//        }
+//        else{
+//            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+//        }
+//
+//    }
 }
--- src/main/java/com/tothenew/journalApp/service/JournalEntryService.java
+++ src/main/java/com/tothenew/journalApp/service/JournalEntryService.java
@@ -1,12 +1,14 @@
 package com.tothenew.journalApp.service;
 
 import com.tothenew.journalApp.entity.JournalEntry;
+import com.tothenew.journalApp.entity.User;
 import com.tothenew.journalApp.repository.JournalEntryRepository;
 import lombok.RequiredArgsConstructor;
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 @RequiredArgsConstructor
@@ -18,15 +20,21 @@ public class JournalEntryService {
 
 
     private final JournalEntryRepository journalEntryRepository;
+    @Autowired
+    private UserService userService;
 
     /* the above 2 lines are basically means that
     * It tells Spring:
 “Hey, I need an object of JournalEntryRepository here. Please create it and give it to me automatically. This is Known as Dependency injection ”
 * without @Autowired we have to write  JournalEntryRepository journalEntryRepository = new JournalEntryRepository(); // Manual way (not preferred in Spring) */
 
-    public void saveEntry(JournalEntry journalEntry)
+    public void saveEntry(JournalEntry journalEntry, String userName)
     {
-        journalEntryRepository.save(journalEntry);
+        User user = userService.findByUserName(userName);
+        journalEntry.setDate(LocalDateTime.now());
+        JournalEntry saved = journalEntryRepository.save(journalEntry);
+        user.getJournalEntries().add(saved);
+        userService.saveUser(user);
     }
 
     public List<JournalEntry> getAll()
