cd7a887e88754fee2160f6fa6753f75f4a6ff113
Zubair7102 <jubaira646@gmail.com>
2025-04-23 00:26:05 +0530

    @Transactional setup

--- src/main/java/com/tothenew/journalApp/JournalApplication.java
+++ src/main/java/com/tothenew/journalApp/JournalApplication.java
@@ -2,8 +2,14 @@ package com.tothenew.journalApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.mongodb.MongoDatabaseFactory;
+import org.springframework.data.mongodb.MongoTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableTransactionManagement
 public class JournalApplication {
 
 	public static void main(String[] args) {
@@ -13,5 +19,11 @@ It boots up the Spring Boot application.
 It creates an ApplicationContext, which manages all the Spring Beans.
  This refers to the main class of your Spring Boot app.*/
 	}
+
+	@Bean
+	public PlatformTransactionManager mongoTransactionManager(MongoDatabaseFactory dbFactory) {
+		return new MongoTransactionManager(dbFactory);
+	}
+
 
 }
