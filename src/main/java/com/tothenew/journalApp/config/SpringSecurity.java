e3cfd2b571d60de30ddcb95a201e7d0993c5ea81
Zubair7102 <jubaira646@gmail.com>
2025-04-29 00:23:52 +0530

    Spring_Security Logic Update

---
+++ src/main/java/com/tothenew/journalApp/config/SpringSecurity.java
@@ -0,0 +1,50 @@
+package com.tothenew.journalApp.config;
+
+import com.tothenew.journalApp.service.UserDetailsServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity // Activates security features like authentication and authorization.
+public class SpringSecurity {
+
+    @Autowired
+    private UserDetailsServiceImpl userDetailsService;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/journal/**", "/user/**").authenticated() // Only authenticated users can access /journal/**
+                        .anyRequest().permitAll() // All other URLs are open
+                )
+                .httpBasic();// Enable basic HTTP authentication
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and().csrf().disable();
+        return http.build(); // Build the Security Filter Chain
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(HttpSecurity http) throws Exception {
+        AuthenticationManagerBuilder authenticationManagerBuilder =
+                http.getSharedObject(AuthenticationManagerBuilder.class);
+        /* Ask Spring Security: "Give me the AuthenticationManagerBuilder object", which helps build AuthenticationManager step-by-step. */
+        authenticationManagerBuilder.userDetailsService(userDetailsService) /* Tell Spring: "Whenever someone logs in, use my custom UserDetailsServiceImpl class to load user info (username, password, roles) from database." */
+                .passwordEncoder(passwordEncoder());
+        return authenticationManagerBuilder.build(); /* Build and return the AuthenticationManager ready to use for login."
+         */
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(); // BCrypt algorithm to encode passwords
+    }
+}
--- src/main/java/com/tothenew/journalApp/controller/UserController.java
+++ src/main/java/com/tothenew/journalApp/controller/UserController.java
@@ -1,11 +1,14 @@
 package com.tothenew.journalApp.controller;
 import com.tothenew.journalApp.entity.JournalEntry;
 import com.tothenew.journalApp.entity.User;
+import com.tothenew.journalApp.repository.UserRepository;
 import com.tothenew.journalApp.service.UserService;
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -21,41 +24,41 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+    @Autowired
+    private UserRepository userRepository;
+
 //    method to get all the Users
-    @GetMapping
-    public List<User> getAllUser(){
-        return userService.getAll();
-    }
+//    @GetMapping
+//    public List<User> getAllUser(){
+//        return userService.getAll();
+//    }
 
 //    Method to add new User
-    @PostMapping
-    public void createUser(@RequestBody User user)
-    {
-        userService.saveUser(user);
-    }
+//    @PostMapping
+//    public void createUser(@RequestBody User user)
+//    {
+//        userService.saveUser(user);
+//    }
 
 //    method to update an existing user
-    @PutMapping("/{userName}")
-    public ResponseEntity<User> updateUser(@RequestBody User user, @PathVariable String userName)
+    @PutMapping
+    public ResponseEntity<User> updateUser(@RequestBody User user)
     {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String userName = authentication.getName();
         User userInDb = userService.findByUserName(userName);
-        if(userInDb != null)
-        {
             userInDb.setUserName(user.getUserName());
             userInDb.setPassword(user.getPassword());
             userService.saveUser(userInDb);
             return new ResponseEntity<>(userInDb, HttpStatus.OK);
-        }
-        else {
-            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-        }
     }
 
 //    Method to delete a user
-    @DeleteMapping("/id/{userId}")
+    @DeleteMapping
     public ResponseEntity<User> deleteUserById(@PathVariable ObjectId userId)
     {
-        userService.deleteById(userId);
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        userRepository.deleteByUserName(authentication.getName());
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
 //        return true;
     }
