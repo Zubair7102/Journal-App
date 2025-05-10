0e26316d211db451d0c6a812c548b3f102864062
Zubair7102 <jubaira646@gmail.com>
2025-05-10 12:22:06 +0530

    JUnit Implement

---
+++ src/test/java/com/tothenew/journalApp/service/UserServiceTest.java
@@ -0,0 +1,48 @@
+package com.tothenew.journalApp.service;
+
+import com.tothenew.journalApp.entity.User;
+import com.tothenew.journalApp.repository.UserRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.AutoConfigureTestEntityManager;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+public class UserServiceTest {
+
+    @Autowired
+    private UserService userService;
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private JournalEntryService journalEntryService;
+
+    @ParameterizedTest
+    @ValueSource(strings = {
+            "Ballu",
+            "Zubair",
+            "Kaisilious"
+    })
+    public void testFindByUserName(String name){
+//        assertEquals(4, 3+1);
+//        assertNotNull(userRepository.findByUserName("Ballu"));
+//        User user = userRepository.findByUserName("Zubair");
+        assertNotNull(userRepository.findByUserName(name));
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "1,1,2",
+            "2,10,12",
+            "3,3,9"
+    })
+    public void test(int a, int b, int expected)
+    {
+        assertEquals(expected,a+b);
+    }
+}
