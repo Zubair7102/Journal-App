c113fe106aa3dd7c18c653920eefcd3fd9927e30
Zubair7102 <jubaira646@gmail.com>
2025-06-22 23:33:27 +0530

    weather service and admin controller updates

--- src/main/java/com/tothenew/journalApp/service/WeatherService.java
+++ src/main/java/com/tothenew/journalApp/service/WeatherService.java
@@ -1,6 +1,8 @@
 package com.tothenew.journalApp.service;
 
 import com.tothenew.journalApp.api.response.WeatherResponse;
+import com.tothenew.journalApp.cache.AppCache;
+import com.tothenew.journalApp.constants.PlaceHolders;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpMethod;
@@ -15,14 +17,14 @@ public class WeatherService {
     @Value("${weather.api.key}")
     private String apiKey;
 
-    public static final String api = "http://api.weatherstack.com/current?access_key=API_KEY&query=CITY";
-
+    @Autowired
+    private AppCache appCache;
     @Autowired
     private RestTemplate restTemplate;
 
     public WeatherResponse getWeather(String city)
     {
-        String finalAPI = api.replace("CITY", city).replace("API_KEY", apiKey);
+        String finalAPI = appCache.appCache.get(AppCache.keys.WEATHER_API.toString()).replace(PlaceHolders.CITY, city).replace(PlaceHolders.API_KEY, apiKey);
          ResponseEntity<WeatherResponse> response =  restTemplate.exchange(finalAPI, HttpMethod.GET, null, WeatherResponse.class);
          return response.getBody();
     }
