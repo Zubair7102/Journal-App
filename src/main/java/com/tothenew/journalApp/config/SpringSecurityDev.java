bfb0d1c3843198cc3a0dde67ca37e874a2b623ef
Zubair7102 <jubaira646@gmail.com>
2025-07-13 20:33:04 +0530

    jwt updates

--- src/main/java/com/tothenew/journalApp/config/SpringSecurityDev.java
+++ src/main/java/com/tothenew/journalApp/config/SpringSecurityDev.java
@@ -1,12 +1,12 @@
 package com.tothenew.journalApp.config;
 
+import com.tothenew.journalApp.filter.JwtFilter;
 import com.tothenew.journalApp.service.UserDetailsServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -14,31 +14,34 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
-@EnableWebSecurity // Activates security features like authentication and authorization.
+@EnableWebSecurity
 @Profile("dev")
 public class SpringSecurityDev {
 
     @Autowired
     private UserDetailsServiceImpl userDetailsService;
 
+    @Autowired
+    private JwtFilter jwtFilter;
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-                .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/journal/**", "/user/**").authenticated()
-                        .requestMatchers("/admin/**").hasRole("ADMIN")// Only authenticated users can access /journal/**
-                        .anyRequest().permitAll() // All other URLs are open
-                )
-                .httpBasic();// Enable basic HTTP authentication
+        http.authorizeHttpRequests(auth -> auth
+                .requestMatchers("/journal/**", "/user/**").authenticated()
+                .requestMatchers("/admin/**").hasRole("ADMIN")
+                .anyRequest().permitAll()
+        );
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and().csrf().disable();
-        return http.build(); // Build the Security Filter Chain
+        http.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
+        return http.build();
     }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder(); // BCrypt algorithm to encode passwords
+        return new BCryptPasswordEncoder();
     }
 
     @Bean
